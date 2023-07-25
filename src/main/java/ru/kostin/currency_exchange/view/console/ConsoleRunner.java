9fcc7266e0c77c66369f2ec4f69b8456909efb0c
Sergey Kostin <serg89981@icloud.com>
2023-07-25 15:10:17 +0300

    timeout for ConsoleRunner

--- src/main/java/ru/kostin/currency_exchange/view/console/ConsoleRunner.java
+++ src/main/java/ru/kostin/currency_exchange/view/console/ConsoleRunner.java
@@ -3,22 +3,32 @@ package ru.kostin.currency_exchange.view.console;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
-import ru.kostin.currency_exchange.exception.BadRequestException;
 
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 @Service
 @RequiredArgsConstructor
 public class ConsoleRunner implements CommandLineRunner {
     private final RequestHandler handler;
 
+
     @Override
     public void run(String... args) {
         Scanner scanner = new Scanner(System.in);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        String[] command = new String[1];
         try {
-            System.out.println(handler.handle(scanner.nextLine()));
-        } catch (BadRequestException e) {
-            System.out.println("Bad Request!");
+            service.submit(
+                    () -> command[0] = scanner.nextLine()
+            ).get(10, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException ignored) {
+        } catch (TimeoutException e) {
+            return;
         }
+        if (command[0].equals("-1")) {
+            return;
+        }
+        System.out.println(handler.handle(command[0]));
     }
 }
